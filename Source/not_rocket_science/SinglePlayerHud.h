3d5e74d076144c48d63f27734a86bf1be57ed94a
Adam Mann <neonmonkey1@hotmail.com>
2024-03-10 16:26:25 -0400

    Setting up HUD base class. Stubbing in show win screen.

---
+++ Source/not_rocket_science/SinglePlayerHud.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "SinglePlayerHud.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOT_ROCKET_SCIENCE_API ASinglePlayerHud : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintImplementableEvent)
+	void ShowWinScreen();	
+};
